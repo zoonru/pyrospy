67cfe6415b40ee348a52a3708aec46de63705281
Alexander Pankratov <s.pankratoff@gmail.com>
2022-04-27 12:28:03 +0300

    Fix autoload when loaded as dependency

--- pyrospy.php
+++ pyrospy.php
@@ -1,6 +1,11 @@
 <?php
 
-require 'vendor/autoload.php';
+$autoloadFile = __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoloadFile)) {
+	require $autoloadFile;
+} else {
+	require __DIR__ . '/../../autoload.php';
+}
 
 use Symfony\Component\Console\Application;
 use Zoon\PyroSpy\Commands\RunCommand;
